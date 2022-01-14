1dca295e3d734aaf0aae6fcdf7f108727072a7da
DJ1TJOO <t.ferb1@gmail.com>
2022-01-14 20:22:55 +0100

    Added package manager to upgrade

--- src/cli.js
+++ src/cli.js
@@ -1,60 +1,54 @@
-import { init } from "./init";
-import { upgrade } from "./upgrade";
-import yargs from "yargs/yargs";
-import fs from "fs";
+import { init } from './init';
+import { upgrade } from './upgrade';
+import yargs from 'yargs/yargs';
+import fs from 'fs';
 
 export async function cli(args) {
-    const argsNoBin = args.slice(2, args.length);
-    if (argsNoBin[0] === "init") {
-        init(args.filter(x => x !== "init"));
-    } else if (argsNoBin[0] === "upgrade") {
-        upgrade(args.filter(x => x !== "upgrade"));
-    } else {
-        if (!argsNoBin[0]) argsNoBin[0] = "--help";
+	const argsNoBin = args.slice(2, args.length);
+	if (argsNoBin[0] === 'init') {
+		init(args.filter((x) => x !== 'init'));
+	} else if (argsNoBin[0] === 'upgrade') {
+		upgrade(args.filter((x) => x !== 'upgrade'));
+	} else {
+		if (!argsNoBin[0]) argsNoBin[0] = '--help';
 
-        const options = yargs(argsNoBin)
-            .usage("Usage: $0 <command> [options]")
+		const options = yargs(argsNoBin)
+			.usage('Usage: $0 <command> [options]')
 
-            .command("init", "Creates a new shapez.io mod project")
-            .example(
-                "$0 init --shapez latest --git",
-                "Creates a new project with the latest shapez.io build and initializes git"
-            )
-            .command("upgrade", "Changes the shapez.io build")
-            .example(
-                "$0 upgrade --shapezRepo https://github.com/DJ1TJOO/shapez.io/tree/modloader-try-again",
-                "Changes the shapez.io build to latest on a custom repo"
-            )
+			.command('init', 'Creates a new shapez.io mod project')
+			.example('$0 init --shapez latest --git', 'Creates a new project with the latest shapez.io build and initializes git')
+			.command('upgrade', 'Changes the shapez.io build')
+			.example('$0 upgrade --shapezRepo https://github.com/DJ1TJOO/shapez.io/tree/modloader-try-again', 'Changes the shapez.io build to latest on a custom repo')
 
-            .alias("s", "shapez")
-            .nargs("s", 1)
-            .describe("s", "Specify shapez.io commit")
+			.alias('s', 'shapez')
+			.nargs('s', 1)
+			.describe('s', 'Specify shapez.io commit')
 
-            .alias("r", "shapezRepo")
-            .nargs("r", 1)
-            .describe("r", "Specify repository to download from")
+			.alias('r', 'shapezRepo')
+			.nargs('r', 1)
+			.describe('r', 'Specify repository to download from')
 
-            .alias("g", "git")
-            .nargs("g", 0)
-            .describe("g", "Initializes git")
+			.alias('g', 'git')
+			.nargs('g', 0)
+			.describe('g', 'Initializes git')
 
-            .alias("i", "install")
-            .nargs("i", 0)
-            .describe("i", "Install all dependencies")
+			.alias('i', 'install')
+			.nargs('i', 0)
+			.describe('i', 'Install all dependencies')
 
-            .alias("y", "yes")
-            .nargs("y", 0)
-            .describe("y", "Skip questions").argv;
+			.alias('y', 'yes')
+			.nargs('y', 0)
+			.describe('y', 'Skip questions').argv;
 
-        const s = fs.createReadStream(options.file);
+		const s = fs.createReadStream(options.file);
 
-        let lines = 0;
-        s.on("data", function (buf) {
-            lines += buf.toString().match(/\n/g).length;
-        });
+		let lines = 0;
+		s.on('data', function (buf) {
+			lines += buf.toString().match(/\n/g).length;
+		});
 
-        s.on("end", function () {
-            console.log(lines);
-        });
-    }
+		s.on('end', function () {
+			console.log(lines);
+		});
+	}
 }
--- src/upgrade.js
+++ src/upgrade.js
@@ -1,97 +1,106 @@
-import arg from "arg";
-import inquirer from "inquirer";
-import { upgradeProject } from "./main";
+import arg from 'arg';
+import inquirer from 'inquirer';
+import { upgradeProject } from './main';
 
 function parseArgumentsIntoOptions(rawArgs) {
-    const args = arg(
-        {
-            "--files": Boolean,
-            "--shapez": String,
-            "--shapez-repo": String,
-            "--install": Boolean,
-            "--yes": Boolean,
-            "-f": "--files",
-            "-s": "--shapez",
-            "-r": "--shapez-repo",
-            "-i": "--install",
-            "-y": "--yes",
-        },
-        {
-            argv: rawArgs.slice(2),
-        }
-    );
-    return {
-        skipPrompts: args["--yes"] || false,
-        updateFiles: args["--files"] || false,
-        shapez: args["--shapez"] || false,
-        shapezRepo: args["--shapez-repo"] || false,
-        runInstall: args["--install"] || false,
-    };
+	const args = arg(
+		{
+			'--files': Boolean,
+			'--shapez': String,
+			'--shapez-repo': String,
+			'--install': Boolean,
+			'--yes': Boolean,
+			'-f': '--files',
+			'-s': '--shapez',
+			'-r': '--shapez-repo',
+			'-i': '--install',
+			'-y': '--yes',
+		},
+		{
+			argv: rawArgs.slice(2),
+		},
+	);
+	return {
+		skipPrompts: args['--yes'] || false,
+		updateFiles: args['--files'] || false,
+		shapez: args['--shapez'] || false,
+		shapezRepo: args['--shapez-repo'] || false,
+		runInstall: args['--install'] || false,
+	};
 }
 
 async function promptForMissingOptions(options) {
-    const defaultShapez = "latest";
-    const defaultShapezRepo = "https://github.com/DJ1TJOO/shapez.io/tree/modloader-try-again";
-    const defaultInstallShapez = true;
-    const defaultUpdateFiles = true;
+	const defaultShapez = 'latest';
+	const defaultShapezRepo = 'https://github.com/DJ1TJOO/shapez.io/tree/modloader-try-again';
+	const defaultInstallShapez = true;
+	const defaultUpdateFiles = true;
 
-    if (options.skipPrompts) {
-        return {
-            shapezRepo: options.shapezRepo || defaultShapezRepo,
-            shapez: options.shapez || defaultShapez,
-            installShapez: options.shapez || defaultInstallShapez,
-            updateFiles: options.updateFiles || defaultUpdateFiles,
-        };
-    }
+	if (options.skipPrompts) {
+		return {
+			shapezRepo: options.shapezRepo || defaultShapezRepo,
+			shapez: options.shapez || defaultShapez,
+			installShapez: options.shapez || defaultInstallShapez,
+			updateFiles: options.updateFiles || defaultUpdateFiles,
+		};
+	}
 
-    const questions = [];
-    if (!options.shapez) {
-        questions.push({
-            type: "confirm",
-            name: "installShapez",
-            message: "Download shapez.io build?",
-            default: defaultInstallShapez,
-        });
+	const questions = [];
+	if (!options.shapez) {
+		questions.push({
+			type: 'confirm',
+			name: 'installShapez',
+			message: 'Download shapez.io build?',
+			default: defaultInstallShapez,
+		});
 
-        questions.push({
-            name: "shapez",
-            message: "Please input the shapez commit hash you want to use:",
-            default: defaultShapez,
-            when: answers => answers.installShapez,
-        });
-    }
+		questions.push({
+			name: 'shapez',
+			message: 'Please input the shapez commit hash you want to use:',
+			default: defaultShapez,
+			when: (answers) => answers.installShapez,
+		});
+	}
 
-    if (!options.updateFiles) {
-        questions.push({
-            type: "confirm",
-            name: "updateFiles",
-            message: "Download and update new project files?",
-            default: defaultUpdateFiles,
-        });
-    }
+	if (!options.updateFiles) {
+		questions.push({
+			type: 'confirm',
+			name: 'updateFiles',
+			message: 'Download and update new project files?',
+			default: defaultUpdateFiles,
+		});
+	}
 
-    if (!options.runInstall) {
-        questions.push({
-            type: "confirm",
-            name: "runInstall",
-            message: "Install all modules?",
-            default: true,
-        });
-    }
+	if (!options.runInstall) {
+		questions.push({
+			type: 'confirm',
+			name: 'runInstall',
+			message: 'Install all modules?',
+			default: true,
+		});
 
-    const answers = await inquirer.prompt(questions);
-    return {
-        shapezRepo: options.shapezRepo || defaultShapezRepo,
-        shapez: options.shapez || answers.shapez,
-        installShapez: options.shapez ? true : answers.installShapez,
-        runInstall: options.runInstall || answers.runInstall,
-        updateFiles: options.updateFiles || answers.updateFiles,
-    };
+		questions.push({
+			type: 'list',
+			name: 'packageManager',
+			message: 'Choose which package manager you want to use:',
+			choices: ['yarn', 'npm'],
+			default: defaultPackageManager,
+		});
+	}
+
+	const answers = await inquirer.prompt(questions);
+	return {
+		shapezRepo: options.shapezRepo || defaultShapezRepo,
+		shapez: options.shapez || answers.shapez,
+		installShapez: options.shapez ? true : answers.installShapez,
+		runInstall: options.runInstall || answers.runInstall,
+		updateFiles: options.updateFiles || answers.updateFiles,
+		packageManager: answers.packageManager,
+	};
 }
 
 export async function upgrade(args) {
-    let options = parseArgumentsIntoOptions(args);
-    options = await promptForMissingOptions(options);
+	let options = parseArgumentsIntoOptions(args);
+	options = await promptForMissingOptions(options);
 
-    await upgradeProject(options);
+	await upgradeProject(options);
 }
