a48aaef64cfdc1f1ac45aa1f115884d1f5492a8d
DJ1TJOO <t.ferb1@gmail.com>
2022-01-16 18:04:50 +0100

    Removed broken constructor

--- template/src/js/main.js
+++ template/src/js/main.js
@@ -1,20 +1,5 @@
 import { Mod } from "shapez/mods/mod";
 
 class ModImpl extends Mod {
-    constructor(app, modLoader) {
-        super(
-            app,
-            {
-                website: "mod_website",
-                author: "mod_author",
-                name: "mod_name",
-                version: "mod_version",
-                id: "mod_id",
-                description: "mod_description",
-            },
-            modLoader
-        );
-    }
-
     init() {}
 }
