15c06ac90b1470fae0f030396023b673466f1860
DJ1TJOO <t.ferb1@gmail.com>
2022-01-16 15:36:44 +0100

    Changed main.js

--- template/src/js/main.js
+++ template/src/js/main.js
@@ -1,22 +1,20 @@
 import { Mod } from "shapez/mods/mod";
 
-registerMod(() => {
-    return class ModImpl extends Mod {
-        constructor(app, modLoader) {
-            super(
-                app,
-                {
-                    website: "mod_website",
-                    author: "mod_author",
-                    name: "mod_name",
-                    version: "mod_version",
-                    id: "mod_id",
-                    description: "mod_description",
-                },
-                modLoader
-            );
-        }
+class ModImpl extends Mod {
+    constructor(app, modLoader) {
+        super(
+            app,
+            {
+                website: "mod_website",
+                author: "mod_author",
+                name: "mod_name",
+                version: "mod_version",
+                id: "mod_id",
+                description: "mod_description",
+            },
+            modLoader
+        );
+    }
 
-        init() {}
-    };
-});
+    init() {}
+}
