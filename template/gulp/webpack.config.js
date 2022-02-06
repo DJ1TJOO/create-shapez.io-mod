da380a48029d00e391d9634f2c3d48cf895d8085
DJ1TJOO <t.ferb1@gmail.com>
2022-02-06 18:57:09 +0100

    Fixed source map

--- template/gulp/webpack.config.js
+++ template/gulp/webpack.config.js
@@ -119,7 +119,7 @@ module.exports = async ({ watch = false }) => {
                 ? [
                       new SourceMapDevToolPlugin({
                           filename: "[file].map",
-                          publicPath: "http://localhost:3010/[name]/",
+                          publicPath: "http://localhost:3010/",
                       }),
                   ]
                 : []),
